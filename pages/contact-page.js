2557adc71bb865bf2cde112c7c8457ad6c47219b
wtnbyuu <watanabe@nn-i-nn.com>
2021-08-07 14:48:41 +0900

    Initial Commit

---
+++ pages/contact-page.js
@@ -0,0 +1,23 @@
+import Layout from "../components/Layout";
+import Image from "next/image";
+
+export default function Contact() {
+  return (
+    <Layout title="Contact">
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <p className="font-bold">Contant info</p>
+        </div>
+        <div className="flex justify-center mt-4">
+          <Image
+            className="rounded-full"
+            src="/avatar.jpg"
+            width={60}
+            height={60}
+            alt="Avatar"
+          />
+        </div>
+      </div>
+    </Layout>
+  );
+}
